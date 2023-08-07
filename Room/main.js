17f04f875224ea1c5065f10b213cfd91ebb4a044
Jaysin <followjaysin@gmail.com>
2023-08-07 08:57:39 -0400

    update

--- Room/main.js
+++ Room/main.js
@@ -2,3 +2,4 @@ import './style.css'
 import Experience from './Experience/Experience.js'
 
 const experience = new Experience(document.querySelector(".experience-canvas"))
+const experience2 = new Experience(document.querySelector(".experience-canvas2"))
